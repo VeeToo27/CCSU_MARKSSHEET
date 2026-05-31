e032b74bb1a72e539b89c64ec299d2e9c9c0f3d7
VeeToo27 <164456019+VeeToo27@users.noreply.github.com>
2026-05-31 21:09:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,14 @@
+import subprocess
+import os
+
+if not os.path.exists("/home/appuser/.cache/ms-playwright"):
+    subprocess.run(["playwright", "install", "chromium"])
+
 from playwright.sync_api import sync_playwright
 
 with sync_playwright() as p:
     browser = p.chromium.launch(headless=True)
     page = browser.new_page()
-
-    def on_response(response):
-        req = response.request
-        if req.resource_type in ["fetch", "xhr"]:
-            print(req.url)
-
-    page.on("response", on_response)
-
     page.goto("https://example.com")
-    page.wait_for_timeout(5000)
-
+    print(page.title())
     browser.close()
